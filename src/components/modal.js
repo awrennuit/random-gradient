037357bd1db917cd4eb002001f173a2a4ad96425
Awren Nuit <awren.nuit@gmail.com>
2022-05-21 09:55:45 -0500

    zoop modal in on page load; import google font; set standard font size; add ability to select gradient code on click

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import "../stylesheets/_modal.scss";
+
 export default function Modal({ gradient, name }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsOpen(true), 500);
+  }, []);
+
+  const selectText = (text) => {
+    if (document.selection) {
+      // for IE
+      const range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(text));
+      range.select();
+    } else if (window.getSelection) {
+      const range = document.createRange();
+      range.selectNode(document.getElementById(text));
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+    }
+  };
+
   return (
-    <div className="modal">
-      <p>Your gradient is called {name}</p>
-      <p>
-        Here it is: <span>{gradient}</span>
+    <div className={`modal ${isOpen ? null : "modal--hidden"}`}>
+      <h1 className="modal__name">{name}</h1>
+      <p
+        id="gradient"
+        className="modal__code"
+        onClick={() => selectText("gradient")}
+      >
+        {gradient}
       </p>
     </div>
   );
